72272d8e541fb98e7901c0cc126221e55c2444df
Denis Gorobzeev <denis.gorobzeev@gmail.com>
2017-03-20 12:01:27 +0200

    add all without VK api

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-
+import User from '../components/User'
+import Page from '../components/Page'
+import * as pageActions from '../actions/PageActions'
 
 class App extends Component {
 	render() {
-		const { name } = this.props.user
-		const {year, photos } = this.props.page
-		return <div>
-				<p> Привет из мира SPA, { name }</p>
-				<p> У тебя { photos.length } фото за {year } год !</p>
-			</div>
+		const { user, page } = this.props
+		const { getPhotos } = this.props.pageActions
+		
+		return <div className='row'>
+			<User name={user.name} />
+			<Page photos={page.photos} year={page.year} getPhotos={getPhotos} fetching={page.fetching} />
+			
+		</div>
 	}
 }
 
-function mapStateToProps (state) {
+function mapStateToProps(state) {
 	return {
 		user: state.user,
 		page: state.page
 	}
 }
 
-export default connect(mapStateToProps) (App)
+function mapDispatchToProps(dispatch) {
+	return {
+		pageActions: bindActionCreators(pageActions, dispatch)
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
